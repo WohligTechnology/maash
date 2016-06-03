5ff23e439767b9b9d4d7499e343fdbf842c95f07
amit-wohlig <amitwohlig@gmail.com>
2016-06-03 16:03:07 +0530

    change

--- www/js/app.js
+++ www/js/app.js
@@ -102,5 +102,5 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     }
   });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('/no-header/select-city');
 });
