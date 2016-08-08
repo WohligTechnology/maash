27a0f5acda8d0f9d27a6eacab5fa9bb05190584b
sukshawohlig <sukshawohlig@gmail.com>
2016-08-08 13:55:56 +0530

    changes

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -143,13 +143,11 @@ angular.module('starter.controllers', [])
       if (data.value) {
         $scope.homeContent = data.data;
         $scope.content = _.groupBy($scope.homeContent, "type");
+
         $scope.smaaashAttract = _.chunk($scope.content.Attraction,1);
-
-      $scope.smaaashNew1= $scope.content["What's new"];
-        $scope.smaaashNew =_.chunk($scope.smaaashNew1,1)
-
-       $scope.smaaashParty1 = $scope.content["Host a Party"];
-         $scope.smaaashParty=_.chunk($scope.smaaashParty1 ,1)
+console.log('ftgyhujikgtyhu',$scope.smaaashAttract);
+        $scope.smaaashNew = $scope.content["What's new"];
+        $scope.smaaashParty = $scope.content["Host a Party"];
         console.log("$scope.smaaashParty", $scope.smaaashParty);
         console.log("$scope.smaaashNew ", $scope.smaaashNew);
         console.log("$scope.smaaashAttract", $scope.smaaashAttract);
