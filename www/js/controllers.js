04ccc7e54f573abacd31d03154acdd340760f401
deshmukhsanket <sanketdeshmukh7206@gmail.com>
2016-08-09 17:30:29 +0530

    Home Page Change

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,284 +1,284 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope,$state, $ionicModal, $timeout, $ionicScrollDelegate,$ionicSideMenuDelegate) {
+.controller('AppCtrl', function($scope, $state, $ionicModal, $timeout, $ionicScrollDelegate, $ionicSideMenuDelegate) {
 
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-  $scope.getCross = "";
-  $scope.whenClose = function() {
-    console.log("clickabe");
-    if ($scope.getCross == "") {
-      $scope.getCross = "m-cross";
-    } else {
-      $scope.getCross = "";
-    }
-  };
-  $scope.closeAll = function(val) {
-    $state.go(val);
-    if($ionicSideMenuDelegate.isOpenLeft())
-    {
-      $ionicSideMenuDelegate.toggleLeft();
-      // $scope.whenClose();
-    }
-    if($ionicSideMenuDelegate.isOpenRight())
-    {
-      $ionicSideMenuDelegate.toggleRight();
-      $scope.whenClose();
-    }
-  };
+    // With the new view caching in Ionic, Controllers are only called
+    // when they are recreated or on app start, instead of every page change.
+    // To listen for when this page is active (for example, to refresh data),
+    // listen for the $ionicView.enter event:
+    //$scope.$on('$ionicView.enter', function(e) {
+    //});
+    $scope.getCross = "";
+    $scope.whenClose = function() {
+        console.log("clickabe");
+        if ($scope.getCross == "") {
+            $scope.getCross = "m-cross";
+        } else {
+            $scope.getCross = "";
+        }
+    };
+    $scope.closeAll = function(val) {
+        $state.go(val);
+        if ($ionicSideMenuDelegate.isOpenLeft()) {
+            $ionicSideMenuDelegate.toggleLeft();
+            // $scope.whenClose();
+        }
+        if ($ionicSideMenuDelegate.isOpenRight()) {
+            $ionicSideMenuDelegate.toggleRight();
+            $scope.whenClose();
+        }
+    };
 
 
 
 
-  $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams) {
-    $ionicScrollDelegate.scrollTop();
-  });
+    $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams) {
+        $ionicScrollDelegate.scrollTop();
+    });
 
 
-  // Form data for the login modal
-  $scope.loginData = {};
+    // Form data for the login modal
+    $scope.loginData = {};
 
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
+    // Create the login modal that we will use later
+    $ionicModal.fromTemplateUrl('templates/login.html', {
+        scope: $scope
+    }).then(function(modal) {
+        $scope.modal = modal;
+    });
 
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
+    // Triggered in the login modal to close it
+    $scope.closeLogin = function() {
+        $scope.modal.hide();
+    };
 
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
+    // Open the login modal
+    $scope.login = function() {
+        $scope.modal.show();
+    };
 
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
+    // Perform the login action when the user submits the login form
+    $scope.doLogin = function() {
+        console.log('Doing login', $scope.loginData);
 
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
-  // scroll class
-  // $(document).ready(function() {
-  //   $(window).scroll(function() {
-  //     var scroll = $(window).scrollTop();
-  //     console.log(scroll);
-  //     if (scroll >= 40) {
-  //       $(".bar-light").addClass("darkHeader");
-  //     } else {
-  //       $(".bar-light").removeClass("darkHeader");
-  //     }
-  //   });
-  // });
+        // Simulate a login delay. Remove this and replace with your login
+        // code if using a login system
+        $timeout(function() {
+            $scope.closeLogin();
+        }, 1000);
+    };
+    // scroll class
+    // $(document).ready(function() {
+    //   $(window).scroll(function() {
+    //     var scroll = $(window).scrollTop();
+    //     console.log(scroll);
+    //     if (scroll >= 40) {
+    //       $(".bar-light").addClass("darkHeader");
+    //     } else {
+    //       $(".bar-light").removeClass("darkHeader");
+    //     }
+    //   });
+    // });
 
 
 })
 
 .controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [{
-    title: 'Reggae',
-    id: 1
-  }, {
-    title: 'Chill',
-    id: 2
-  }, {
-    title: 'Dubstep',
-    id: 3
-  }, {
-    title: 'Indie',
-    id: 4
-  }, {
-    title: 'Rap',
-    id: 5
-  }, {
-    title: 'Cowbell',
-    id: 6
-  }];
+    $scope.playlists = [{
+        title: 'Reggae',
+        id: 1
+    }, {
+        title: 'Chill',
+        id: 2
+    }, {
+        title: 'Dubstep',
+        id: 3
+    }, {
+        title: 'Indie',
+        id: 4
+    }, {
+        title: 'Rap',
+        id: 5
+    }, {
+        title: 'Cowbell',
+        id: 6
+    }];
 })
 
 .controller('HomeCtrl', function($scope, $stateParams, $ionicSlideBoxDelegate) {
-  $scope.homeslider = [
-    'img/banners/banner.jpg',
-    'img/banners/banner.jpg',
-    'img/banners/banner.jpg',
-    'img/banners/banner.jpg'
-  ];
+    $scope.homeslider = [
+        'img/banners/banner.jpg',
+        'img/banners/banner.jpg',
+        'img/banners/banner.jpg',
+        'img/banners/banner.jpg'
+    ];
+    $scope.showSignUp = false;
+    if ($.jStorage.get("user") || _.isEmpty($.jStorage.get("user"))) {
+        $scope.showSignUp = true;
+    } else {
+        $scope.showSignUp = false;
+    }
 })
 
 .controller('SelectCityCtrl', function($scope, $stateParams, MyServices, $state) {
 
-  MyServices.getCity(function(data) {
-    $scope.getCity = _.chunk(data.data, 2);
-    console.log('$scope.getCity', $scope.getCity);
-  })
+    MyServices.getCity(function(data) {
+        $scope.getCity = _.chunk(data.data, 2);
+        console.log('$scope.getCity', $scope.getCity);
+    })
+    $scope.selectCity = function(city) {
+        $.jStorage.set("mycity", city);
+        $state.go("noheader.signup");
+    }
 
-  // $scope.getCityName=function(cityName){
-  //   $.jStorage.set("city",cityName);
-  //   $scope.city=$.jStorage.get("city").name;
-  // console.log("  $scope.city",  $scope.city);
-  // }
-
-
-
-
+    // $scope.getCityName=function(cityName){
+    //   $.jStorage.set("city",cityName);
+    //   $scope.city=$.jStorage.get("city").name;
+    // console.log("  $scope.city",  $scope.city);
+    // }
 })
-
-
-
-
-
-
-
 
 .controller('SelectAvatarCtrl', function($scope, $stateParams) {
 
-  })
-  .controller('BuyCtrl', function($scope, $stateParams) {
+})
 
-  })
-  .controller('EventCtrl', function($scope, $stateParams) {
+.controller('BuyCtrl', function($scope, $stateParams) {
 
-  })
-  .controller('ExploreSmaaashCtrl', function($scope, $stateParams, $ionicSlideBoxDelegate, MyServices) {
+})
 
-    $scope.nextSlide = function(val) {
-      console.log($ionicSlideBoxDelegate.$getByHandle(val).slidesCount());
-      console.log($ionicSlideBoxDelegate.$getByHandle(val).currentIndex());
-      if ($ionicSlideBoxDelegate.$getByHandle(val).slidesCount()-2 <= $ionicSlideBoxDelegate.$getByHandle(val).currentIndex()) {
-        $ionicSlideBoxDelegate.$getByHandle(val).slide(0);
-      } else {
-        $ionicSlideBoxDelegate.$getByHandle(val).next();
-      }
-    };
-    $scope.options = {
-      loop: true
-    };
-    $scope.disableSwipe = function() {
-      $ionicSlideBoxDelegate.enableSlide(false);
-    };
-    $scope.smaaashAttract = [];
-    $scope.smaaashNew = [];
+.controller('EventCtrl', function($scope, $stateParams) {
 
-    $scope.smaaashParty = [];
+})
 
-    MyServices.getHomeContent(function(data) {
-      if (data.value) {
-        $scope.homeContent = data.data;
-        $scope.content = _.groupBy($scope.homeContent, "type");
+.controller('ExploreSmaaashCtrl', function($scope, $stateParams, $ionicSlideBoxDelegate, MyServices) {
 
-        $scope.smaaashAttract = $scope.content.Attraction;
+        $scope.nextSlide = function(val) {
+            console.log($ionicSlideBoxDelegate.$getByHandle(val).slidesCount());
+            console.log($ionicSlideBoxDelegate.$getByHandle(val).currentIndex());
+            if ($ionicSlideBoxDelegate.$getByHandle(val).slidesCount() - 2 <= $ionicSlideBoxDelegate.$getByHandle(val).currentIndex()) {
+                $ionicSlideBoxDelegate.$getByHandle(val).slide(0);
+            } else {
+                $ionicSlideBoxDelegate.$getByHandle(val).next();
+            }
+        };
+        $scope.options = {
+            loop: true
+        };
+        $scope.disableSwipe = function() {
+            $ionicSlideBoxDelegate.enableSlide(false);
+        };
+        $scope.smaaashAttract = [];
+        $scope.smaaashNew = [];
 
-        $scope.smaaashNew = $scope.content["What's new"];
-        console.log(JSON.stringify($scope.content["What's new"]));
+        $scope.smaaashParty = [];
 
-        $scope.smaaashParty = $scope.content["Host a Party"];
-        console.log("$scope.smaaashParty", $scope.smaaashParty);
-        console.log("$scope.smaaashNew ", $scope.smaaashNew);
-        console.log("$scope.smaaashAttract", $scope.smaaashAttract);
-      }
+        MyServices.getHomeContent(function(data) {
+            if (data.value) {
+                $scope.homeContent = data.data;
+                $scope.content = _.groupBy($scope.homeContent, "type");
 
-    });
+                $scope.smaaashAttract = $scope.content.Attraction;
 
-    // $scope.smaaashNew = [
-    //   'img/new.png',
-    //   'img/new.png',
-    //   'img/new.png',
-    //   'img/new.png',
-    //   'img/new.png'
-    // ];
-    // $scope.smaaashAttract = [
-    //   'img/attract.png',
-    //   'img/attract.png',
-    //   'img/attract.png',
-    //   'img/attract.png',
-    //   'img/attract.png'
-    // ];
-    // $scope.smaaashParty = [
-    //   'img/party.png',
-    //   'img/party.png',
-    //   'img/party.png',
-    //   'img/party.png',
-    //   'img/party.png'
-    // ];
+                $scope.smaaashNew = $scope.content["What's new"];
+                console.log(JSON.stringify($scope.content["What's new"]));
 
-  })
-  .controller('PaymentCtrl', function($scope, $stateParams) {
-    $scope.accordion = function(val) {
-      console.log($scope.showAccordion);
-      console.log(val);
-      if (val == $scope.showAccordion) {
-        $scope.showAccordion = 0;
-      } else {
-        if (val == 1) {
-          $scope.showAccordion = 1;
-        } else if (val == 2) {
-          $scope.showAccordion = 2;
-        } else if (val == 3) {
-          $scope.showAccordion = 3;
-        } else if (val == 4) {
-          $scope.showAccordion = 4;
-        } else if (val == 5) {
-          $scope.showAccordion = 5;
-        } else {
-          $scope.showAccordion = 0;
+                $scope.smaaashParty = $scope.content["Host a Party"];
+                console.log("$scope.smaaashParty", $scope.smaaashParty);
+                console.log("$scope.smaaashNew ", $scope.smaaashNew);
+                console.log("$scope.smaaashAttract", $scope.smaaashAttract);
+            }
+
+        });
+
+        // $scope.smaaashNew = [
+        //   'img/new.png',
+        //   'img/new.png',
+        //   'img/new.png',
+        //   'img/new.png',
+        //   'img/new.png'
+        // ];
+        // $scope.smaaashAttract = [
+        //   'img/attract.png',
+        //   'img/attract.png',
+        //   'img/attract.png',
+        //   'img/attract.png',
+        //   'img/attract.png'
+        // ];
+        // $scope.smaaashParty = [
+        //   'img/party.png',
+        //   'img/party.png',
+        //   'img/party.png',
+        //   'img/party.png',
+        //   'img/party.png'
+        // ];
+
+    })
+    .controller('PaymentCtrl', function($scope, $stateParams) {
+        $scope.accordion = function(val) {
+            console.log($scope.showAccordion);
+            console.log(val);
+            if (val == $scope.showAccordion) {
+                $scope.showAccordion = 0;
+            } else {
+                if (val == 1) {
+                    $scope.showAccordion = 1;
+                } else if (val == 2) {
+                    $scope.showAccordion = 2;
+                } else if (val == 3) {
+                    $scope.showAccordion = 3;
+                } else if (val == 4) {
+                    $scope.showAccordion = 4;
+                } else if (val == 5) {
+                    $scope.showAccordion = 5;
+                } else {
+                    $scope.showAccordion = 0;
+                }
+            }
+            console.log($scope.showAccordion);
+            console.log(val);
         }
-      }
-      console.log($scope.showAccordion);
-      console.log(val);
-    }
 
-  })
-  .controller('DirectionCtrl', function($scope, $stateParams) {
+    })
+    .controller('DirectionCtrl', function($scope, $stateParams) {
 
-  })
-  .controller('AccountCtrl', function($scope, $stateParams, $ionicPopup) {
-    $scope.getPlan = function() {
-      $scope.checkPlan = $ionicPopup.show({
-        templateUrl: 'templates/modal/headline.html',
-        scope: $scope
-      });
-    };
-    $scope.closePopup = function() {
-      $scope.checkPlan.close();
-    }
-  })
-  .controller('ConfirmOrderCtrl', function($scope, $stateParams) {
+    })
+    .controller('AccountCtrl', function($scope, $stateParams, $ionicPopup) {
+        $scope.getPlan = function() {
+            $scope.checkPlan = $ionicPopup.show({
+                templateUrl: 'templates/modal/headline.html',
+                scope: $scope
+            });
+        };
+        $scope.closePopup = function() {
+            $scope.checkPlan.close();
+        }
+    })
+    .controller('ConfirmOrderCtrl', function($scope, $stateParams) {
 
-  })
-  .controller('RechargeCtrl', function($scope, $stateParams, $ionicPopup) {
-    $scope.popHeadline = function() {
-      $scope.headlienPop = $ionicPopup.show({
-        templateUrl: 'templates/modal/headline.html',
-        scope: $scope
-      });
-    }
-    $scope.closePopup = function() {
-      $scope.headlienPop.close();
-    }
-  })
+    })
+    .controller('RechargeCtrl', function($scope, $stateParams, $ionicPopup) {
+        $scope.popHeadline = function() {
+            $scope.headlienPop = $ionicPopup.show({
+                templateUrl: 'templates/modal/headline.html',
+                scope: $scope
+            });
+        }
+        $scope.closePopup = function() {
+            $scope.headlienPop.close();
+        }
+    })
 
-.controller('SignupCtrl', function($scope, $stateParams, $ionicPopup, $state) {
+.controller('SignupCtrl', function($scope, $stateParams, $ionicPopup, $state, MyServices) {
     var ionicpop = "";
     $scope.oneTimepswd = function() {
-      ionicpop = $ionicPopup.show({
-        templateUrl: 'templates/modal/otp.html',
-        scope: $scope
-      });
+        ionicpop = $ionicPopup.show({
+            templateUrl: 'templates/modal/otp.html',
+            scope: $scope
+        });
     }
     $scope.toAvatar = function() {
-      ionicpop.close();
-      $state.go("noheader.avatar")
+        ionicpop.close();
+        $state.go("noheader.avatar")
     };
 
     $scope.userForm = {};
@@ -289,11 +289,26 @@ angular.module('starter.controllers', [])
     //   }
     //
     // }
+    $scope.userSignup = function(input) {
+        console.log("userSignup");
+        MyServices.signUp(input, function(data) {
+            console.log(data);
+            if (data.value) {
 
+            } else {
 
-  })
-  .controller('NoHeaderCtrl', function($scope, $stateParams) {
+            }
+            // if()
+            //
+            // }else {
+            //
+            // }
+        })
+    }
+})
 
-  })
+.controller('NoHeaderCtrl', function($scope, $stateParams) {
+
+})
 
 .controller('PlaylistCtrl', function($scope, $stateParams) {});
--- www/js/services.js
+++ www/js/services.js
@@ -1,36 +1,40 @@
-// var adminurl = "http://192.168.1.102:1337/";   //local
-var adminurl = "http://104.197.84.255/";  //server
-// var imgpath = adminurl + "uploadfile/getupload?file=";
-var imgurl = adminurl + "upload/";
-var imgpath = imgurl + "readFile";
-var uploadurl = imgurl;
+ var adminurl = "http://192.168.0.123:1337/"; //local
+ // var adminurl = "http://104.197.84.255/";  //server
+ // var imgpath = adminurl + "uploadfile/getupload?file=";
+ var imgurl = adminurl + "upload/";
+ var imgpath = imgurl + "readFile";
+ var uploadurl = imgurl;
 
 
-angular.module('starter.services', [])
+ angular.module('starter.services', [])
 
-.factory('MyServices', function($http) {
+ .factory('MyServices', function($http) {
 
-  return {
+   return {
 
-    getHomeContent: function(callback) {
+     getHomeContent: function(callback) {
 
-      return $http({
-        url: adminurl + 'exploresmash/getHomeContent',
-        method: "POST",
-        withCredentials: true
-      }).success(callback);
-    },
-    getCity: function(callback) {
-            $http({
-            url: adminurl + 'city/getAllCityByOrder',
-            method: 'POST',
-            withCredentials: true
-        }).success(callback);
-    },
-
-
-
-
-
-  };
-});
+       return $http({
+         url: adminurl + 'exploresmash/getHomeContent',
+         method: "POST",
+         withCredentials: true
+       }).success(callback);
+     },
+     getCity: function(callback) {
+       $http({
+         url: adminurl + 'city/getAllCityByOrder',
+         method: 'POST',
+         withCredentials: true
+       }).success(callback);
+     },
+     signUp: function(data, callback) {
+       console.log("signUp");
+       $http({
+         url: adminurl + 'user/signup',
+         method: 'POST',
+         withCredentials: true,
+         data: data
+       }).success(callback);
+     }
+   };
+ });
