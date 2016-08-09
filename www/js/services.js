b1cc5e83dd18c764dd528ff05ceedea3ed244162
amit-wohlig <amitwohlig@gmail.com>
2016-08-09 17:39:14 +0530

    changes done :grin:

--- www/js/services.js
+++ www/js/services.js
@@ -1,5 +1,5 @@
- var adminurl = "http://192.168.0.123:1337/"; //local
- // var adminurl = "http://104.197.84.255/";  //server
+ // var adminurl = "http://192.168.0.123:1337/"; //local
+ var adminurl = "http://104.197.84.255/";  //server
  // var imgpath = adminurl + "uploadfile/getupload?file=";
  var imgurl = adminurl + "upload/";
  var imgpath = imgurl + "readFile";
